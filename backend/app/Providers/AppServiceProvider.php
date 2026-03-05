5fcf79a0fbf4a273967960087dcb76287e655a92
jcarloshg <carlosj12336@gmail.com>
2026-03-04 22:55:25 -0600

    create the implement the provider for parking spaces

--- backend/app/Providers/AppServiceProvider.php
+++ backend/app/Providers/AppServiceProvider.php
@@ -2,21 +2,21 @@
 
 namespace App\Providers;
 
+use App\Models\ParkingSpace;
+use App\Policies\ParkingSpacePolicy;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
+    protected $policies = [
+        ParkingSpace::class => ParkingSpacePolicy::class,
+    ];
+
     public function register(): void
     {
         //
     }
 
-    /**
-     * Bootstrap any application services.
-     */
     public function boot(): void
     {
         //
